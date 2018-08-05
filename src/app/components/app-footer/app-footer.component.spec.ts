a3379a6b0db3e41f5acd25f73db4d651887e12d4
Adler Coelho Santos <adler.coelho.santos@icloud.com>
2018-08-05 09:25:29 -0300

    Fix appFooterComponent spec.

--- src/app/components/app-footer/app-footer.component.spec.ts
+++ src/app/components/app-footer/app-footer.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppFooterComponent } from './app-footer.component';
 
 describe('AppFooterComponent', () => {
+
   let component: AppFooterComponent;
   let fixture: ComponentFixture<AppFooterComponent>;
 
@@ -16,10 +17,9 @@ describe('AppFooterComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(AppFooterComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
